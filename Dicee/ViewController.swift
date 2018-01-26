b19155e2b0cd6b0790bef5f412abe8dcabddaff5
David Porter <davidbrianporter@gmail.com>
2018-01-26 09:12:28 -0800

    added random function to roll dice

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0;
+    var randomDiceIndex2 : Int = 0;
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -25,8 +28,13 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex2)
+        print(randomDiceIndex1)
     }
     
 }
 
-// git commits not showing			
+// git commits not showing	1/26/2018 needed email set
