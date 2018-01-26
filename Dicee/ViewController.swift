7f66c180fa638a7040c632ff7c866ccaf672fd19
Captain <captain@Captains-Mac.local>
2018-01-25 18:20:27 -0800

    xcode project added

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Captain on 1/25/18.
+//  Copyright © 2018 Captain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
