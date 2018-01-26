54ec3a0dd7727b19fdf12cf2c3e6ae93d4194e4f
captain <captain@captains-Mac.local>
2018-01-26 08:33:47 -0800

    first swift 4 code added

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
